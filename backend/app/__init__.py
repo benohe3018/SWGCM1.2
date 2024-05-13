69bbd6d76f50ac08b7bf85e8f94fc456da69e854
benohe3018 <48292108+benohe3018@users.noreply.github.com>
2024-05-13 15:12:50 -0600

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,48 +1,40 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_cors import CORS  # Importa CORS
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+import os
 
-
-# Inicializa las extensiones pero aún sin ninguna app vinculada
 db = SQLAlchemy()
 migrate = Migrate()
 
-# Importa todos los modelos
-from .models import * 
+from .models import *
 
 from .routes.auth import auth_bp
 from .routes.medicos import medicos_bp
 from .routes.usuarios import usuarios_bp
 
 def create_app():
-    # Crea una instancia de la aplicación Flask
     app = Flask(__name__)
 
-    # Configuración básica de la aplicación
-    app.config['SECRET_KEY'] = 'Barcelona3018?'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Barc3lona3018?@localhost:5432/swgcm'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default-secret-key')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:Barc3lona3018?@localhost:5432/swgcm')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
-    # Inicializa las extensiones con la aplicación Flask
+
     db.init_app(app)
     migrate.init_app(app, db)
-    jwt = JWTManager(app)  # Inicializa JWTManager con la aplicación Flask
-    
-    # Configura CORS para permitir solicitudes desde el dominio del frontend
-    CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+    jwt = JWTManager(app)
 
-    # Registro de blueprints
+    CORS(app, resources={r"/api/*": {"origins": "*"}})  # Ajusta las CORS según tus necesidades
+
     app.register_blueprint(auth_bp, url_prefix='/api')
     app.register_blueprint(medicos_bp, url_prefix='/api')
-    app.register_blueprint(usuarios_bp, url_prefix='/api') 
+    app.register_blueprint(usuarios_bp, url_prefix='/api')
 
     @app.cli.command("create_db")
     def create_db():
         with app.app_context():
             db.create_all()
-               
-    
+
     return app
 

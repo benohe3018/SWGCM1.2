dd22f28d41f5ff7ba69b0ab0e128b56804b409d5
benohe3018 <48292108+benohe3018@users.noreply.github.com>
2024-05-13 19:18:08 -0600

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -12,6 +12,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default-secret-key')
+    # Asegúrate de que la URL de la base de datos comienza con 'postgresql://'
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:Barc3lona3018?@localhost:5432/swgcm')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -21,6 +22,7 @@ def create_app():
 
     CORS(app, resources={r"/api/*": {"origins": "*"}})  # Ajusta las CORS según tus necesidades
 
+    # Importa e inicializa los blueprints
     from .routes.auth import auth_bp
     from .routes.medicos import medicos_bp
     from .routes.usuarios import usuarios_bp

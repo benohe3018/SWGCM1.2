b8de9e3c3233236d6e6c273a9b4e0ad1818533b3
benohe3018 <48292108+benohe3018@users.noreply.github.com>
2024-05-13 19:06:56 -0600

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'backend')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 from app import create_app
 
 app = create_app()

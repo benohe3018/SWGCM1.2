26ab437910ffe098502bb513b1d765fb24c2fdcb
benohe3018 <topeteplay89@gmail.com>
2024-05-28 15:31:50 -0600

    actualización a Administrador.js y su css

--- frontend/src/components/Administrador.js
+++ frontend/src/components/Administrador.js
@@ -1,18 +1,21 @@
 // Administrador.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Administrador.css'; // Asegúrate de crear este archivo CSS
 import logoIMSS from '../images/LogoIMSS.jpg';
 
 const Administrador = () => {
   return (
-    <div className="administrador-page">
-      <header className="administrador-header">
+    <div className="admin-page">
+      <header className="admin-header">
         <img src={logoIMSS} alt="Logo IMSS" className="header-logo" />
-        <h1 className="welcome-message">Bienvenido al Sistema de Gestión de Citas Médicas</h1>
-        <h2 className="department-name">Departamento de Resonancia Magnética - HGR #46</h2>
+        <h1 className="welcome-message-admin">Bienvenido al Sistema de Gestión de Citas Médicas</h1>
+        <h2 className="department-name-admin">Departamento de Resonancia Magnética - HGR #46</h2>
       </header>
-      <h3 className="en-construction-message">Esta página está en construcción.</h3>
-      
+      <h3 className="en-construction-message-admin">Esta página está en construcción.</h3>
+      <Link to="/">
+        <button className="admin-button">Volver al inicio de sesión</button>
+      </Link>
     </div>
   );
 };

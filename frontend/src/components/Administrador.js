5bab36837679f550a60d99fc213cb6cab38ca0ad
benohe3018 <topeteplay89@gmail.com>
2024-05-28 15:20:42 -0600

    Diferencias Administrator y admin

--- frontend/src/components/Administrador.js
+++ frontend/src/components/Administrador.js
@@ -1,6 +1,5 @@
 // Administrador.js
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Administrador.css'; // Asegúrate de crear este archivo CSS
 import logoIMSS from '../images/LogoIMSS.jpg';
 
@@ -12,10 +11,8 @@ const Administrador = () => {
         <h1 className="welcome-message">Bienvenido al Sistema de Gestión de Citas Médicas</h1>
         <h2 className="department-name">Departamento de Resonancia Magnética - HGR #46</h2>
       </header>
-      <h3 className="en-construccion-message">Esta página está en construcción.</h3>
-      <Link to="/dashboard-root">
-        <button className="administrator-button">Volver al inicio de sesión</button>
-      </Link>
+      <h3 className="en-construction-message">Esta página está en construcción.</h3>
+      
     </div>
   );
 };

01a6cc26beb5ace48ca107da8f429062349a11ec
benohe3018 <topeteplay89@gmail.com>
2024-05-28 15:01:59 -0600

    Actualización al message-content

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -1,17 +1,21 @@
 //Admin.js
 import React from "react";
 import './Admin.css';
+import { Link } from 'react-router-dom';
 import logoIMSS from '../images/LogoIMSS.jpg';
 
 const Admin = () => {
     return (
-        <div>
+        <div className="administrador-page">
             <header className="admin-header">
                 <img src={logoIMSS} alt="Logo IMSS" className="header-logo" />
                 <h1 className="welcome-message">Bienvenido al Sistema de Gestión de Citas Médicas</h1>
                 <h2 className="department-name">Departamento de Resonancia Magnética - HGR #46</h2>
             </header>
             <h3 className="en-construccion-message">Esta página está en construcción.</h3>
+            <Link to="/dashboard-root">
+                <button className="administrator-button">Volver al inicio de sesión</button>
+            </Link>
         </div>
     );
 };

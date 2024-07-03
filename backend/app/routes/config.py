b9fc079aa1d510e5ab379229e0ebed50944328b5
benohe3018 <topeteplay89@gmail.com>
2024-07-03 08:30:49 -0600

    Configuración de Argon2 y ajustes en auth.py

---
+++ backend/app/routes/config.py
@@ -0,0 +1,11 @@
+from argon2 import PasswordHasher
+
+ARGON2_TIME_COST = 2
+ARGON2_MEMORY_COST = 102400
+ARGON2_PARALLELISM = 8
+
+ph = PasswordHasher(
+    time_cost=ARGON2_TIME_COST,
+    memory_cost=ARGON2_MEMORY_COST,
+    parallelism=ARGON2_PARALLELISM
+)

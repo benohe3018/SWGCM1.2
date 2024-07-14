790a79a7fe6e264e0715e8c06d119b66aa5dd17c
benohe3018 <topeteplay89@gmail.com>
2024-07-14 11:02:34 -0600

    vuelta para atras de citas.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -16,6 +16,7 @@ from .routes.auth import auth_bp
 from .routes.medicos import medicos_bp
 from .routes.usuarios import usuarios_bp
 from .routes.estudios import estudios_bp
+from .routes.citas import citas_bp
 from .routes.unidades_medicas import unidades_medicas_bp  # Nuevo import
 from .routes.hospitales import hospitales_bp  # Nuevo import
 from .routes.pacientes_prueba import pacientes_prueba_bp 
@@ -39,6 +40,7 @@ def create_app():
     app.register_blueprint(medicos_bp, url_prefix='/api')
     app.register_blueprint(usuarios_bp, url_prefix='/api')
     app.register_blueprint(estudios_bp, url_prefix='/api')
+    app.register_blueprint(citas_bp, url_prefix='/api')
     app.register_blueprint(unidades_medicas_bp, url_prefix='/api')  # Registro nuevo
     app.register_blueprint(hospitales_bp, url_prefix='/api')  # Registro nuevo
     app.register_blueprint(pacientes_prueba_bp, url_prefix='/api')

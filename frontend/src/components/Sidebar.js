2b53e873bf8cbbe25a7f50f0748d17776c4ae29b
benohe3018 <topeteplay89@gmail.com>
2024-07-19 07:26:26 -0600

    optimizacion de la sidebar, colocacion de menús deslegables para funciones CRUD en el modulo de medicos

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,14 +1,38 @@
 // src/components/Sidebar.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Sidebar.css';
 
 const Sidebar = () => {
+  const [medicosOpen, setMedicosOpen] = useState(false);
+
+  const handleMouseEnter = () => {
+    setMedicosOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setMedicosOpen(false);
+  };
+
   return (
     <div className="sidebar">
       <h2>Gestión de Citas</h2>
       <ul>
-        <li><Link to="/crud-medicos">Módulo de Médicos</Link></li>
+        <li 
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          Módulo de Médicos
+          {medicosOpen && (
+            <ul className="submenu">
+              <li><Link to="/create-medico">Capturar Nuevo Médico</Link></li>
+              <li><Link to="/read-medico">Ver Médicos</Link></li>
+              <li><Link to="/update-medico">Actualizar Registro de Médico</Link></li>
+              <li><Link to="/delete-medico">Borrar Registro de Médico</Link></li>
+              <li><Link to="/">Salir</Link></li>
+            </ul>
+          )}
+        </li>
         <li><Link to="/crud-usuarios">Módulo de Usuarios</Link></li>
         <li><Link to="/informes-medicos">Módulo de Informes</Link></li>
         <li><Link to="/gestion-citas">Módulo de Citas</Link></li>
@@ -20,3 +44,4 @@ const Sidebar = () => {
 }
 
 export default Sidebar;
+

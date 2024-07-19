d5d4c767fcfe6749afa6f4ddce4d5c4f204d4a2d
benohe3018 <topeteplay89@gmail.com>
2024-07-19 12:12:52 -0600

    optimizacion de la sidebar, colocacion de menús deslegables para funciones CRUD en el modulo de medicosv10

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -3,20 +3,26 @@ import { Link } from 'react-router-dom';
 import './Sidebar.css';
 
 const Sidebar = () => {
-  const [openSubmenu, setOpenSubmenu] = useState(false);
+  const [openSubmenu, setOpenSubmenu] = useState({
+    medicos: false,
+    usuarios: false
+  });
 
-  const handleSubmenuToggle = () => {
-    setOpenSubmenu(!openSubmenu);
+  const handleSubmenuToggle = (menu) => {
+    setOpenSubmenu(prevState => ({
+      ...prevState,
+      [menu]: !prevState[menu]
+    }));
   };
 
   return (
     <div className="sidebar">
-      <h2>Bienvenido</h2>
+      <h2>Gestión de Citas</h2>
       <ul>
-        <li onClick={handleSubmenuToggle}>
+        <li onClick={() => handleSubmenuToggle('medicos')}>
           <span>Módulo de Médicos</span>
-          <span className={`arrow ${openSubmenu ? 'open' : ''}`}>&#9660;</span>
-          {openSubmenu && (
+          <span className={`arrow ${openSubmenu.medicos ? 'open' : ''}`}>&#9660;</span>
+          {openSubmenu.medicos && (
             <ul className="submenu">
               <li><Link to="/create-medico">Capturar Nuevo Médico</Link></li>
               <li><Link to="/read-medico">Ver Médicos</Link></li>
@@ -26,7 +32,19 @@ const Sidebar = () => {
             </ul>
           )}
         </li>
-        <li><Link to="/crud-usuarios">Módulo de Usuarios</Link></li>
+        <li onClick={() => handleSubmenuToggle('usuarios')}>
+          <span>Módulo de Usuarios</span>
+          <span className={`arrow ${openSubmenu.usuarios ? 'open' : ''}`}>&#9660;</span>
+          {openSubmenu.usuarios && (
+            <ul className="submenu">
+              <li><Link to="/create-usuario">Capturar Nuevo Usuario</Link></li>
+              <li><Link to="/read-usuario">Ver Usuarios</Link></li>
+              <li><Link to="/update-usuario">Actualizar Registro de Usuario</Link></li>
+              <li><Link to="/delete-usuario">Borrar Registro de Usuario</Link></li>
+              <li><Link to="/">Salir</Link></li>
+            </ul>
+          )}
+        </li>
         <li><Link to="/informes-medicos">Módulo de Informes</Link></li>
         <li><Link to="/gestion-citas">Módulo de Citas</Link></li>
         <li><Link to="/estudios-radiologicos">Modulo de Estudios Radiológicos</Link></li>
@@ -37,6 +55,7 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+
 
 
 

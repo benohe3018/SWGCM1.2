3fc003c8bb649c3e93783ce222bdde49e148ef15
benohe3018 <topeteplay89@gmail.com>
2024-07-31 20:27:29 -0600

    Implementacion del modulo EspecialidadesMedicas en el frontend

--- frontend/src/components/especialidadesService.js
+++ frontend/src/components/especialidadesService.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://your-api-url.com/api'; // Reemplaza con la URL de tu API
+const API_URL = process.env.REACT_APP_API_URL; 
 
 export const getEspecialidades = async () => {
     try {

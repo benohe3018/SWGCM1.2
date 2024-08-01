ca0bc629a486a803a307d46ade3d7c1d72840ce7
benohe3018 <topeteplay89@gmail.com>
2024-08-01 08:11:18 -0600

    Actualización a FormularioEspecialidad.js

--- frontend/src/components/FormularioEspecialidad.js
+++ frontend/src/components/FormularioEspecialidad.js
@@ -3,7 +3,7 @@ import './FormularioEspecialidad.css';
 
 const FormularioEspecialidad = ({ modo, especialidad, onSubmit, onCancel, mensaje, error }) => {
   const [nombreEspecialidad, setNombreEspecialidad] = useState(especialidad ? especialidad.nombre_especialidad : '');
-  const [formError, setFormError] = useState('');
+  const [formError, alert] = useState('');
 
   useEffect(() => {
     if (modo === 'crear') {
@@ -19,11 +19,11 @@ const FormularioEspecialidad = ({ modo, especialidad, onSubmit, onCancel, mensaj
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!nombreEspecialidad) {
-      setFormError('El nombre de la especialidad es obligatorio');
+      alert('El nombre de la especialidad es obligatorio');
       return;
     }
     if (!validarNombreEspecialidad(nombreEspecialidad)) {
-      setFormError('El nombre de la especialidad no es válido, ejemplo, capture "Cirugía"');
+      alert('El nombre de la especialidad no es válido, ejemplo, capture "Cirugía"');
       return;
     }
     onSubmit({ nombre_especialidad: nombreEspecialidad });
@@ -33,9 +33,9 @@ const FormularioEspecialidad = ({ modo, especialidad, onSubmit, onCancel, mensaj
     const { value } = e.target;
     if (validarNombreEspecialidad(value) || value === '') {
       setNombreEspecialidad(value);
-      setFormError('');
+      alert('');
     } else {
-      setFormError('El nombre de la especialidad no es válido');
+      alert('El nombre de la especialidad no es válido');
     }
   };
 

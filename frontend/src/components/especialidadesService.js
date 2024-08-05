da104d27437a67cafc4f2cca06682e5830058aba
benohe3018 <topeteplay89@gmail.com>
2024-08-05 08:34:37 -0600

    Implementación de cambios en especialidadesService.js

--- frontend/src/components/especialidadesService.js
+++ frontend/src/components/especialidadesService.js
@@ -6,7 +6,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 export const getEspecialidades = async () => {
     try {
-        const response = await axios.get(`${API_URL}/especialidades`);
+        const response = await axios.get(`${API_URL}/api/especialidades`);
         return response.data;
     } catch (error) {
         console.error("Error al obtener las especialidades médicas:", error);
@@ -16,7 +16,7 @@ export const getEspecialidades = async () => {
 
 export const createEspecialidad = async (especialidad) => {
     try {
-        const response = await axios.post(`${API_URL}/especialidades`, especialidad);
+        const response = await axios.post(`${API_URL}/api/especialidades`, especialidad);
         return response.data;
     } catch (error) {
         console.error("Error al crear la especialidad médica:", error);
@@ -26,7 +26,7 @@ export const createEspecialidad = async (especialidad) => {
 
 export const updateEspecialidad = async (id, especialidad) => {
     try {
-        const response = await axios.put(`${API_URL}/especialidades/${id}`, especialidad);
+        const response = await axios.put(`${API_URL}/api/especialidades/${id}`, especialidad);
         return response.data;
     } catch (error) {
         console.error("Error al actualizar la especialidad médica:", error);
@@ -36,7 +36,7 @@ export const updateEspecialidad = async (id, especialidad) => {
 
 export const deleteEspecialidad = async (id) => {
     try {
-        const response = await axios.delete(`${API_URL}/especialidades/${id}`);
+        const response = await axios.delete(`${API_URL}/api/especialidades/${id}`);
         return response.data;
     } catch (error) {
         console.error("Error al eliminar la especialidad médica:", error);

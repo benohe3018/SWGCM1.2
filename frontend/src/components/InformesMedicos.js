3b3a2e85b6840a1b46c7379d1bef8999c0c5c246
benohe3018 <topeteplay89@gmail.com>
2024-09-05 00:17:06 -0600

    2do commit a informes

--- frontend/src/components/InformesMedicos.js
+++ frontend/src/components/InformesMedicos.js
@@ -1,9 +1,25 @@
-//InformesMedicos.js
-import React from "react";
-import './InformesMedicos.css'; // Asegúrate de crear este archivo CSS
+import React, { useState, useEffect } from "react";
+import './InformesMedicos.css';
 import logoIMSS from '../images/LogoIMSS.jpg';
 
 const InformesMedicos = () => {
+    const [citas, setCitas] = useState([]);
+    const hospitalId = "ID_DEL_HOSPITAL"; // Reemplaza con el ID del hospital deseado
+
+    const fetchCitas = async (hospitalId) => {
+        try {
+            const response = await fetch(`/api/pacientes_prueba?hospital_envia=${hospitalId}`);
+            const data = await response.json();
+            setCitas(data);
+        } catch (error) {
+            console.error("Error fetching citas:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchCitas(hospitalId);
+    }, [hospitalId]);
+
     return (
         <div>
             <header className="informes-citas-header">
@@ -11,7 +27,14 @@ const InformesMedicos = () => {
                 <h1 className="welcome-message">Bienvenido al Sistema de Gestión de Citas Médicas</h1>
                 <h2 className="department-name">Departamento de Resonancia Magnética - HGR #46</h2>
             </header>
-            <h3 className="en-construccion-message">Esta página está en construcción.</h3>
+            <div>
+                {citas.map(cita => (
+                    <div key={cita.id}>
+                        <p>{cita.nombre_completo}</p>
+                        {/* Muestra más detalles de la cita aquí */}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };

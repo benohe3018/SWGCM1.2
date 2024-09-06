ff4ca233aaea55a57a7cda4120623da394d4a9e3
benohe3018 <topeteplay89@gmail.com>
2024-09-06 17:25:54 -0600

    Actualizacion a Reporteestudios

--- frontend/src/components/ReporteEstudios.js
+++ frontend/src/components/ReporteEstudios.js
@@ -79,7 +79,7 @@ const ReporteEstudios = () => {
                     <option value="nombre_estudio">Nombre del Estudio</option>
                     <option value="descripcion_estudio">Descripción del Estudio</option>
                 </select>
-                <button onClick={generatePDF}>Generar PDF</button>
+                <button className="reporte-estudios-radiologicos" onClick={generatePDF}> Imprimir Reporte en PDF</button>
             </div>
             <div className="tabla-estudios-container">
                 <table className="tabla-estudios">

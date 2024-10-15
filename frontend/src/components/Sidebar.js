9557ae8747aafe2dbf8723dcdd1a3903c102c74f
benohe3018 <topeteplay89@gmail.com>
2024-10-15 06:30:46 -0600

    actualizacion a la sidebar quitando el botón de cerrar sesión

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -105,11 +105,11 @@ const Sidebar = () => {
             Cambiar Sesión
           </button>
         </li>
-        <li>
+        {/*<li>
           <button onClick={handleExitSystem} className="sidebar-button">
             Cerrar Página
           </button>
-        </li>
+        </li>*/}
       </ul>
       {user && (
         <div className="active-user">

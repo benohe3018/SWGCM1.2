f1664ff5d28938ac0a58c39b88e8558b5fa73f1e
benohe3018 <topeteplay89@gmail.com>
2024-10-23 07:33:54 -0600

    Actualizacion al nombre del médico

--- frontend/src/components/GestionCitas.js
+++ frontend/src/components/GestionCitas.js
@@ -379,8 +379,8 @@ const handleEditarPaciente = async (pacienteEditado) => {
                   <option value="">Seleccione un médico</option>
                   {medicos.map((medico) => (
                     <option key={medico.id_medico} value={medico.id_medico}>
-                      {medico.nombre_medico} 
-                    </option>
+              {`${medico.nombre_medico} ${medico.apellido_paterno_medico} ${medico.apellido_materno_medico}`}
+              </option>
                   ))}
                 </select>
               </td>

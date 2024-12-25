03f80a6503d1eb1f2aa91ab0a21f22dce3b28e4c
benohe3018 <topeteplay89@gmail.com>
2024-12-25 08:46:23 -0600

    eliminacion de nuevas claves recaptcha

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,23 +1,15 @@
-import bcrypt
-import requests
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token
 from datetime import timedelta
-import os
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError, HashingError
 from ..models import Usuario, db
 from .encryption import decrypt_data as decrypt_password
 from .auth_middleware import token_required, role_required
-from .config import ph, SECRET_KEY, ARGON2_TIME_COST, ARGON2_MEMORY_COST, ARGON2_PARALLELISM
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 import base64
-
-# Obtención de variables de entorno para reCAPTCHA
-api_key = os.getenv('RECAPTCHA_API_KEY')
-site_key = os.getenv('RECAPTCHA_SITE_KEY')
-project_id = os.getenv('RECAPTCHA_PROJECT_ID')
+import os
 
 # Creación del blueprint de autenticación
 auth_bp = Blueprint('auth', __name__)
@@ -43,19 +35,12 @@ def generate_token(identity, role):
     return create_access_token(identity=identity, expires_delta=expires, additional_claims=additional_claims)
 
 # Función para hashear la contraseña usando Argon2
-def hash_password(password):
-    try:
-        hashed = ph.hash(password)
-        return hashed
-    except HashingError as e:
-        raise ValueError(f"Error al generar el hash de la contraseña: {str(e)}")
-
-# Función para verificar el hash de la contraseña
+ph = PasswordHasher()
 def check_password_hash(stored_hash, password):
     try:
         ph.verify(stored_hash, password)
         return True
-    except (VerifyMismatchError, ValueError) as e:
+    except (VerifyMismatchError, ValueError):
         return False
 
 # Ruta para el login de usuarios
@@ -64,22 +49,6 @@ def login():
     data = request.get_json()
     username = data.get('nombre_usuario')
     encrypted_password = data.get('password')
-    captcha = data.get('captcha')
-
-    # Verificación del CAPTCHA
-    recaptcha_response = requests.post(f'https://recaptchaenterprise.googleapis.com/v1/projects/{project_id}/assessments?key={api_key}', json={
-        'event': {
-            'token': captcha,
-            'expectedAction': 'LOGIN',
-            'siteKey': site_key,
-        }
-    })
-    recaptcha_data = recaptcha_response.json()
-
-    if 'event' not in recaptcha_data or 'riskAnalysis' not in recaptcha_data:
-        return jsonify({"message": "CAPTCHA inválido"}), 401
-    if recaptcha_data['event']['expectedAction'] != 'LOGIN' or recaptcha_data['riskAnalysis']['score'] < 0.5:
-        return jsonify({"message": "CAPTCHA inválido"}), 401
 
     # Intento de login
     try:
@@ -96,43 +65,3 @@ def login():
             return jsonify({"message": "Credenciales inválidas"}), 401
     except Exception as e:
         return jsonify({"message": f"Error durante el proceso de login: {str(e)}"}), 500
-
-# Ruta para el registro de usuarios
-@auth_bp.route('/register', methods=['POST'])
-@token_required
-@role_required(['root'])
-def register():
-    data = request.get_json()
-    username = data.get('nombre_usuario')
-    password = data.get('password')
-    role = data.get('rol')
-    nombre_real = data.get('nombre_real')
-    apellido_paterno = data.get('apellido_paterno')
-    apellido_materno = data.get('apellido_materno')
-    matricula = data.get('matricula')
-
-    if Usuario.query.filter_by(nombre_usuario=username).first():
-        return jsonify({"message": "El nombre de usuario ya existe"}), 400
-
-    hashed_password = hash_password(password)
-
-    new_user = Usuario(
-        nombre_usuario=username,
-        contrasena=hashed_password,
-        rol=role,
-        nombre_real=nombre_real,
-        apellido_paterno=apellido_paterno,
-        apellido_materno=apellido_materno,
-        matricula=matricula
-    )
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({"message": "Usuario registrado exitosamente en la base de datos"}), 201
-
-# Ruta protegida de ejemplo
-@auth_bp.route('/ruta_protegida', methods=['GET'])
-@token_required
-@role_required(['Admin', 'root'])
-def ruta_protegida():
-    return jsonify({"message": "Acceso a ruta protegida concedido"}), 200
